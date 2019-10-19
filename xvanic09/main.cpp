3fbcec3faf12ec828f6632ae937f15d379a008cc
Jozef Vanický <jozef.vanicky@gmail.com>
2019-10-19 17:50:36 +0200

    Files ready for code

--- xvanic09/main.cpp
+++ xvanic09/main.cpp
@@ -1,6 +1,18 @@
+//
+//  main.cpp
+//  xvanic09
+//
+//  Created by Jozef Vanický on 19/10/2019.
+//  Copyright © 2019 Jozef Vanický. All rights reserved.
+//
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    printf("Hey");
     return 0;
 }
