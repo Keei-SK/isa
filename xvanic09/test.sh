4580d29bf4b91554c9af1b352cbac5258fd2a517
Jozef Vanický <jozef.vanicky@gmail.com>
2019-11-18 21:37:11 +0100

    Tests are implemented.

--- xvanic09/test.sh
+++ xvanic09/test.sh
@@ -3,7 +3,7 @@ mkdir -pv tests
 
 test_standard() { #$1 - name, $2 - server
     dig +short $1 @$2 > tests/$1.ref 2>/dev/null
-    ./dns -r -s $2 $1 | grep $1 | cut -d "," -f 5 | grep "\S" | sed -e 's/^ *//g' > tests/$1.out 2>/dev/null
+    ./dns -r -s $2 $1 | grep "IN," | cut -d "," -f 5 | grep "[[:space:]]" | sed -e 's/^ *//g' > tests/$1.out 2>/dev/null
     diff -c tests/$1.out tests/$1.ref > /dev/null 2>/dev/null
     result=$?
     rm -rf tests/$1.*
@@ -17,7 +17,7 @@ test_standard() { #$1 - name, $2 - server
 
 test_ipv6_standard() { #$1 - name, $2 - server
     dig +short AAAA $1 @$2 > tests/$1.ref 2>/dev/null
-    ./dns -r -6 -s $2 $1 | grep $1 | cut -d "," -f 5 | grep "\S" | sed -e 's/^ *//g' > tests/$1.out 2>/dev/null
+    ./dns -r -6 -s $2 $1 | grep "IN," | cut -d "," -f 5 | grep "[[:space:]]" | sed -e 's/^ *//g' > tests/$1.out 2>/dev/null
     diff -c tests/$1.out tests/$1.ref > /dev/null 2>/dev/null
     result=$?
     rm -rf tests/$1.*
@@ -32,10 +32,10 @@ test_ipv6_standard() { #$1 - name, $2 - server
 
 test_reverse() { #$1 - name, $2 - server
     dig +short -x $1 @$2 > tests/$1.ref 2>/dev/null
-    ./dns -r -x -s $2 $1 | grep $1 | cut -d "," -f 5 | grep "\S" | sed -e 's/^ *//g' > tests/$1.out 2>/dev/null
+    ./dns -r -x -s $2 $1 | grep "IN," | cut -d "," -f 5 | grep "[[:space:]]" | sed -e 's/^ *//g' > tests/$1.out 2>/dev/null
     diff -c tests/$1.out tests/$1.ref > /dev/null 2>/dev/null
     result=$?
-    #rm -rf tests/$1.*
+    rm -rf tests/$1.*
     printf $1
     printf " PTR"
     if [ ${result} = 0 ]; then
@@ -47,12 +47,16 @@ test_reverse() { #$1 - name, $2 - server
 
 SERVER=kazi.fit.vutbr.cz
 test_standard "fit.vut.cz" $SERVER
+test_standard "www.fit.vut.cz" $SERVER
 test_standard "merlin.fit.vutbr.cz" $SERVER
-test_standard "merlin.fit.vut.cz" $SERVER
+test_standard "google.com" $SERVER
+test_standard "dcvision.cz" $SERVER
 
 test_ipv6_standard "fit.vut.cz" $SERVER
+test_ipv6_standard "www.fit.vut.cz" $SERVER
 test_ipv6_standard "merlin.fit.vutbr.cz" $SERVER
-test_ipv6_standard "merlin.fit.vut.cz" $SERVER
+test_ipv6_standard "google.com" $SERVER
 
 test_reverse "147.229.0.1" $SERVER
-#test_reverse "::1" $SERVER
+test_reverse "::1" $SERVER
+test_reverse "2a00:1450:4014:80d::200e" $SERVER
